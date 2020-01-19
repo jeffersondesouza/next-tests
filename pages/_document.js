a53a7cab444e83d39c769021b7d1df0ad2160269
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-01-19 11:13:03 -0300

    ssr

--- pages/_document.js
+++ pages/_document.js
@@ -2,31 +2,28 @@ import { ServerStyleSheet } from "styled-components";
 import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(context) {
-    const { renderPage } = context;
-
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = renderPage;
-
+    const originalRenderPage = ctx.renderPage;
     try {
-      renderPage = () =>
+      ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
         });
+      const initialProps = await Document.getInitialProps(ctx);
 
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      });
-
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      });
-    } catch (error) {
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
       sheet.seal();
     }
-
-    // Returns an object like: { html, head, errorHtml, chunks, styles }
-    return renderPage();
   }
 
   render() {
